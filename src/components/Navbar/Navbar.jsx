ddab7410e3d4f3ebc68ad89d86643211dbd71782
LhyetevDev <foschi246@gmail.com>
2022-09-06 16:22:05 -0300

    desafio menu

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import './Navbar.css'
+import logo from '../../img/frame.svg';
+
+function Navbar() {
+    return (
+    <header>
+        <picture className="logo">
+            <a href="index.html"><img src={logo} alt="" /></a>
+        </picture>
+        <nav className="navbar">
+            <ul className="menu">
+                <li className="menu-item">
+                    <a href="/#" className="menu-link">Productos</a>
+                </li>
+                <li className="menu-item">
+                    <a href="/#" className="menu-link">Nosotros</a>
+                </li>
+                <li className="menu-item">
+                    <a href="/#" className="menu-link">Redes</a>
+                </li>
+                <li className="menu-item">
+                    <a href="/#" className="menu-link">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    );
+}
+
+export default Navbar;
