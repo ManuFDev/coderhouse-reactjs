d66bc2cac9f1cf0dc52bad91fddf2787599c6874
LhyetevDev <foschi246@gmail.com>
2022-09-12 18:19:25 -0300

    desafio componente 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import "./App.css";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import Navbar from "./components/Navbar/Navbar";
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+
+      <main className="main-container">
+        <ItemListContainer
+          img="https://picsum.photos/id/1021/367/267"
+          description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore velit nemo pariatur error odit laboriosam odio. Magnam vitae illo minima vel maiores voluptate porro dolorem nisi ipsum dicta, qui enim."
+          buttonText="Comprar"
+        />
+        <ItemListContainer
+          img="https://picsum.photos/id/1033/367/267"
+          description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore velit nemo pariatur error odit laboriosam odio. Magnam vitae illo minima vel maiores voluptate porro dolorem nisi ipsum dicta, qui enim."
+          buttonText="Comprar"
+        />
+        <ItemListContainer
+          img="https://picsum.photos/id/1050/367/267"
+          description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore velit nemo pariatur error odit laboriosam odio. Magnam vitae illo minima vel maiores voluptate porro dolorem nisi ipsum dicta, qui enim."
+          buttonText="Comprar"
+        />
+      </main>
     </div>
   );
 }
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,24 @@
+import "./ItemListContainer.css";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+function ItemListContainer(props) {
+    return (
+        <div className="productos-container">
+            <Card style={{ width: "30rem" }} className='card'>
+                <Card.Img variant="top" src={props.img} />
+                <Card.Body className='card-body'>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Text className="card-description">
+                        {props.description}
+                    </Card.Text>
+                    <Button variant="primary" className="card-button">{props.buttonText}</Button>
+                </Card.Body>
+            </Card>
+        </div>
+
+    );
+}
+
+export default ItemListContainer;
