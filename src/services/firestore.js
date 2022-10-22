1c1dee81f1c3867ea82f4a239a6cb1ce8d387a46
LhyetevDev <foschi246@gmail.com>
2022-10-22 16:38:29 -0300

    nuevo cambio .env

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -12,12 +12,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0lmy_TQe6swucbHFyuFLAQ0Ei5ryTKRg",
-    authDomain: "react-funkoo.firebaseapp.com",
-    projectId: "react-funkoo",
-    storageBucket: "react-funkoo.appspot.com",
-    messagingSenderId: "508773953825",
-    appId: "1:508773953825:web:ea7ae24ece98098d25ce60"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
